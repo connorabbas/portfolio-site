b361f1129530f175bbf12352a8bc7411437b2052
connorabbas <connora@jdsindustries.com>
2022-01-21 14:44:50 -0600

    dev work

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -59,7 +59,7 @@ class ContactController
             // If CAPTCHA is successfully completed...
             try {
                 //Recipients
-                $mail->setFrom('connor@connor-abbas.com', "Connor's Site");
+                $mail->setFrom('eqostlmy@eqo.stl.mybluehost.me', "Connor's Site");
                 $mail->addAddress('abbasconnor@gmail.com', 'Connor Abbas'); //Add a recipient
                 $mail->addReplyTo('abbasconnor@gmail.com', 'Information');
 
