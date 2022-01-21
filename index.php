3793b551c21a6c6944b421869c15a492df4e2a4a
connorabbas <connora@jdsindustries.com>
2022-01-21 13:03:06 -0600

    new

--- index.php
+++ index.php
@@ -7,6 +7,13 @@ error_reporting(E_ALL);
 require_once('./app/env.php');
 require_once('./app/globals.php');
 
+if (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on" && BASE_DIR == '/') {
+    //Tell the browser to redirect to the HTTPS URL.
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+    //Prevent the rest of the script from executing.
+    exit;
+}
+
 // Autoload Classes
 spl_autoload_register(
     function ($class) {
